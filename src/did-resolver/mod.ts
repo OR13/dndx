7574cad20350d2e4da6be6761a2781dc927f64ae
Orie Steele <orie@transmute.industries>
2020-06-21 17:18:05 -0500

    🦕 🚀

--- src/did-resolver/mod.ts
+++ src/did-resolver/mod.ts
@@ -1,7 +1,10 @@
-export function did_url_to_http_url(url: string): string {
+export function get_module(url: string): string {
   const github_user = url.split("did:github:")[1].split("/")[0];
   const remaining_path = url.split(`did:github:${github_user}/`)[1];
-  //   console.log(github_user);
-  //   console.log(remaining_path);
   return `https://raw.githubusercontent.com/${github_user}/${remaining_path}`;
+}
+
+export function get_did_doc(url: string): string {
+  const github_user = url.split("did:github:")[1].split("/")[0];
+  return `https://raw.githubusercontent.com/${github_user}/ghdid/master/index.jsonld`;
 }
--- src/did-resolver/mod_test.ts
+++ src/did-resolver/mod_test.ts
@@ -1,10 +1,21 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
-import { did_url_to_http_url } from "./mod.ts";
+import { get_module, get_did_doc } from "./mod.ts";
 
 const { test } = Deno;
 
-test("convert path to raw github user content http url", () => {
+test("use did url to load module", () => {
   const url_path = "/did:github:OR13/dndx/master/mod.ts";
-  assertEquals(did_url_to_http_url(url_path), "");
+  assertEquals(
+    get_module(url_path),
+    "https://raw.githubusercontent.com/OR13/dndx/master/mod.ts",
+  );
+});
+
+test("use did url to load did document", () => {
+  const url_path = "/did:github:OR13";
+  assertEquals(
+    get_did_doc(url_path),
+    "https://raw.githubusercontent.com/OR13/ghdid/master/index.jsonld",
+  );
 });
